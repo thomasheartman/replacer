1dff70b5ac4fb0f95711d9db965e6c0be9815cc1
Thomas Heartman <thomas.heartman+git@gmail.com>
2020-11-22 14:42:58 +0100

    Mock out proof of concept for cleaner rewrite.
    This splits out the main logic into a few smaller methods. Separates
    concerns much better.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use handlebars::{Handlebars, TemplateRenderError};
 use log::{debug, error, info, warn};
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
+use std::io::Write;
 use std::{collections::HashMap, fmt};
 use std::{
     ffi::OsStr,
@@ -37,6 +38,8 @@ enum ProgramError {
     FileNotFound(PathBuf),
     ReadFailed(PathBuf),
     RenderError(String),
+    MissingKey(String),
+    InvalidTemplate,
     CannotOpenFileForWriting(PathBuf),
     CannotCreateOutputDirectories(PathBuf),
 }
@@ -49,16 +52,17 @@ impl fmt::Display for ProgramError {
                 format!("Couldn't deserialize {:?} into the expected format.", path)
             }
             ProgramError::RenderError(description) => {
-                format!(
-                    "Render error: {} This is likely due to a missing key in the replacements file.",
-                    description
-                )
+                format!("Template Render error: {}.", description)
             }
             ProgramError::CannotOpenFileForWriting(path) => {
                 format!("Couldn't open output file {:?}.", path)
             }
             ProgramError::CannotCreateOutputDirectories(path) => {
                 format!("Failed to create directory {:?}", path)
+            }
+            ProgramError::MissingKey(msg) => msg.clone(),
+            ProgramError::InvalidTemplate => {
+                "The provided template file is invalid and cannot be parsed correctly.".to_string()
             }
         };
         write!(f, "{}", msg)
@@ -102,6 +106,93 @@ fn go(opts: &Opt) -> Result<PathBuf, ProgramError> {
 
     let mut handlebars = Handlebars::new();
     handlebars.set_strict_mode(true);
+
+    handlebars.register_template_source("input", &mut input);
+    let template_render_result = handlebars.render("input", &replacements);
+
+    struct Configuration {
+        template: File,
+        mappings: HashMap<String, String>,
+        output_file: PathBuf,
+    }
+    fn parse_input_files(opts: &Opt) -> Result<Configuration, ProgramError> {
+        let mut template = open_file(&opts.input_file)?;
+        let mappings: HashMap<String, String> = deserialize(&opts.replacements_file)?;
+        let config: Config = deserialize(&opts.config_file)?;
+        let filename = opts.input_file.as_path().file_name().unwrap_or_else(|| {
+            let default_name = "output";
+            warn!(
+            "Unable to generate an output filename based on the the input file; using {} instead.",
+            &default_name
+        );
+            OsStr::new(default_name)
+        });
+
+        let output_file = Path::new(&config.output_dir.join(&filename)).to_path_buf();
+
+        Ok(Configuration {
+            template,
+            mappings,
+            output_file,
+        })
+    }
+
+    struct RenderResult {
+        result: String,
+        output_file: PathBuf,
+    }
+    fn render_template(mut config: Configuration) -> Result<RenderResult, ProgramError> {
+        let mut handlebars = Handlebars::new();
+        handlebars.set_strict_mode(true);
+
+        handlebars.register_template_source("input", &mut config.template);
+        handlebars
+            .render("input", &config.mappings)
+            .map_err(|e| {
+                if e.desc.starts_with("Variable") {
+                    ProgramError::MissingKey(e.desc)
+                } else if e.desc.starts_with("Template not found") {
+                    ProgramError::InvalidTemplate
+                } else {
+                    ProgramError::RenderError(e.desc)
+                }
+            })
+            .map(|result| RenderResult {
+                result,
+                output_file: config.output_file,
+            })
+    }
+
+    fn write_template_file(
+        RenderResult {
+            result,
+            output_file,
+        }: RenderResult,
+    ) -> Result<PathBuf, ProgramError> {
+        info!("Creating necessary directories.");
+
+        if let Some(parent_dir) = &output_file.as_path().parent() {
+            DirBuilder::new()
+                .recursive(true)
+                .create(&parent_dir)
+                .map_err(|_| {
+                    ProgramError::CannotCreateOutputDirectories(parent_dir.to_path_buf())
+                })?;
+        };
+
+        OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(&output_file)
+            .and_then(|mut f| f.write(&result.as_bytes()))
+            .map_err(|_| ProgramError::CannotOpenFileForWriting(output_file.clone()))?;
+
+        Ok(output_file)
+    }
+
+    // parse_input_files(&opts)
+    //     .and_then(render_template(&mut input))
+    //     .and_then(write_template_file)?;
 
     info!("Creating necessary directories.");
     DirBuilder::new()
